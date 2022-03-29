b8e7976bf52ae5adb2aca9aaac10852b0101cb27
Lexyth <Lexyth@users.noreply.github.com>
2022-03-29 22:49:50 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
-public class Main {
-    public static void main (String[] args) {
-        
-    }
+import runtax.RuleSet;
+import runtax.BasicRuleSet;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    BasicRuleSet basic = BasicRuleSet.instance();
+    RuleSet main = RuleSet.instance();
+    System.out.println(main.toString());
+    System.out.println(basic.toString());
+
+    System.out.println(basic.get("letter"));
+  }
 }
